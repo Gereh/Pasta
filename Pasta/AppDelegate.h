95f61839d1ee03953039f317a5476472016266b9
Amin <M.amin.ansari@gmail.com>
2015-02-28 11:01:37 +0330

    init
    init

---
+++ Pasta/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pasta
+//
+//  Created by Amin on 2/28/15.
+//  Copyright (c) 2015 Reza Sazegarnezhad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pasta/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Pasta
+//
+//  Created by Amin on 2/28/15.
+//  Copyright (c) 2015 Reza Sazegarnezhad. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
